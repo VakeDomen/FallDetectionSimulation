6a4ad50aa1fcfc335c0319da921b2c294093943a
VakeDomen <vake.domen94@gmail.com>
2023-06-12 09:51:25 +0200

    initial commit

---
+++ Assets/gridinstatiator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridinstatiator : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public int rows = 10;
+    public int cols = 10;
+    public float tileSize = 0.3f;  // Size of the tile in meters (30cm)
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        for (int x = 0; x < cols; x++)
+        {
+            for (int z = 0; z < rows; z++)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(x * tileSize, -0.49f, z * tileSize), Quaternion.identity);
+                tile.transform.parent = this.transform;
+
+                // Set i and j indices on tile
+                scr tileScript = tile.GetComponent<scr>();
+                if (tileScript != null)
+                {
+                    tileScript.SetIndices(x, z);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scr.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour
+{
+    private Renderer objectRenderer;
+    private Color initialColor;
+    public int i;
+    public int j;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        initialColor = objectRenderer.material.color;
+    }
+
+    void OnCollisionEnter(Collision collision)
+{
+        // Calculate total impulse due to this collision
+        Vector3 totalImpulse = collision.impulse;
+    
+        // Project this onto the 'down' direction (which is -transform.up in Unity's coordinate system)
+        Vector3 downwardImpulse = Vector3.Project(totalImpulse, -transform.up);
+    
+        // To get force from impulse you need to divide by time. 
+        // However, as Unity applies physics in fixed time steps, this division would give you average force during that time step.
+        Vector3 downwardForce = downwardImpulse / Time.fixedDeltaTime;
+    
+        // Now print the downwardForce to the console
+        if (downwardForce.y != 0.0f) {
+
+            Debug.Log("Downward force: " + downwardForce);
+        }
+    }
+
+
+
+    // This function gets called when the object stops colliding with something
+    void OnCollisionExit(Collision collision)
+    {
+        // revert back to the initial color
+        objectRenderer.material.color = initialColor;
+    }
+
+    public void SetIndices(int i, int j)
+    {
+        this.i = i;
+        this.j = j;
+    }
+}
